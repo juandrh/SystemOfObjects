a0d075b8c4ee3a4657b355f77d3dc555fa263529
Juan Del Rio <52029674+juandrh@users.noreply.github.com>
2022-08-31 18:00:19 +0200

    Add files via upload

---
+++ System03/c/Global.cpp
@@ -0,0 +1,110 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Global.h"
+#include "ForceField.h"
+#include "UtilsLib.h"
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+#include "Kismet/GameplayStatics.h"
+#include "Math/Transform.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+// Sets default values
+AGlobal::AGlobal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (AgentType == 1){
+	static ConstructorHelpers::FObjectFinder<UClass> AgentBP (TEXT("Blueprint'/Game/Blueprints/BP_Agent.BP_Agent_C'")); 
+		if (AgentBP.Object){
+        AgentClass = AgentBP.Object;    
+    }  
+	} else {
+	static ConstructorHelpers::FObjectFinder<UClass> AgentBP (TEXT("Blueprint'/Game/Blueprints/BP_Agent2.BP_Agent2_C'")); 
+		if (AgentBP.Object){
+        AgentClass = AgentBP.Object;    
+    }
+
+	}
+	
+	
+
+
+}
+
+// Called when the game starts or when spawned
+void AGlobal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+	FActorSpawnParameters SpawnInfo; 
+	TArray<AAgent*> Agents;
+
+	FVector InitLocation = FVector(EscenarioSize/2-400.0f, EscenarioSize/2-200.0f, 50.0f);
+	FRotator InitRotation = FRotator(0.0f, 0.0f, 0.0f);
+	TargetLocation = FVector(EscenarioSize/2+400.0f, EscenarioSize/2-200.0f, 50.0f);
+	 
+	for (int i=0; i < Num_Agents; i++ )
+	{
+	
+	 InitLocation = FVector(
+		(EscenarioSize-1000)*FMath::FRand()+500,
+		(EscenarioSize-1000)*FMath::FRand()+500,
+		50.0f); 
+	
+	InitRotation = FRotator(0.0f, 360*FMath::FRand(),0.0f );
+	Agents.Add(GetWorld()->SpawnActor<AAgent>(AgentClass, InitLocation, InitRotation, SpawnInfo)); 
+	}
+
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AgentClass, FoundAgents);
+
+}
+
+// Called every frame
+void AGlobal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//UE_LOG(LogTemp, Warning,TEXT("t->   %f : %f "), TargetLocation.X,TargetLocation.Y);
+	for (AActor* actor : FoundAgents)	
+	{
+		agent = Cast<AAgent>(actor);		
+		agent->ApplyForce(FieldIntesitive * FField->GetCellItem(agent->GetActorLocation()));		
+		agent-> AutoMove();	
+		if(seek){	agent->Seek(TargetLocation);  }
+		if(flee){	agent->Flee(TargetLocation);  }
+		agent->Boundaries(EscenarioSize,BoudaryMargin);
+		agent->Update(); 		
+		agent->RotateBody();
+		
+	}
+
+}
+
+
+// Get location of the line Trace hit in the world
+// Function called by left click mouse button from Blueprint
+ void AGlobal::GetLineTraceHitLocation(FVector vector, int operation)
+{
+	if (vector.X < BoudaryMargin) { vector.X=BoudaryMargin;};
+	if (vector.X > EscenarioSize - BoudaryMargin) { vector.X=EscenarioSize - BoudaryMargin;};
+	if (vector.Y < BoudaryMargin) { vector.Y=BoudaryMargin;};
+	if (vector.Y > EscenarioSize - BoudaryMargin) { vector.Y=EscenarioSize - BoudaryMargin;};
+	
+	TargetLocation = vector;	
+	if (operation == 0){ seek = true;}
+	else{flee = true;}
+
+
+} 
+
+void AGlobal::DisableSeek(){
+
+	seek = false;
+	flee = false;
+
+}
