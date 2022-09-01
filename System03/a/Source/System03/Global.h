8c52a11dcfcfd9c6c580a9ee8b355fb0e04e1bf7
Juan Del Rio <52029674+juandrh@users.noreply.github.com>
2022-09-01 07:42:32 +0200

    Add files via upload

---
+++ System03/a/Source/System03/Global.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Global.generated.h"
+
+UCLASS()
+class SYSTEM03_API AGlobal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGlobal();
+	UPROPERTY (EditAnywhere, Category = "Propiedades")
+	TSubclassOf<class AAgent> AgentClass;
+	UPROPERTY (EditAnywhere, Category = "Propiedades")
+	FVector TargetLocation;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION (BlueprintCallable,meta = (DisplayName = "Get Line Trace Hit Location"))
+	void GetLineTraceHitLocation(FVector vector);
+
+private:
+
+	int Num_Agents = 1;
+
+};
