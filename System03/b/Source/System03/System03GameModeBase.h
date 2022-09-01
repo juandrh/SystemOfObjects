b0dbf6e64e87946b11b9872435ede07badb4aaba
Juan Del Rio <52029674+juandrh@users.noreply.github.com>
2022-09-01 07:47:37 +0200

    Add files via upload

---
+++ System03/b/Source/System03/System03GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "System03GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SYSTEM03_API ASystem03GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
